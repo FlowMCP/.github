3cb82041c49a7a3ce3d71db200e241748deadc0e
αηdr3α5 <AndreasBanholzer@Me.com>
2025-06-30 16:26:22 +0200

    Update index.mjs dynamic insert

--- index.mjs
+++ index.mjs
@@ -5,11 +5,13 @@ const btg = new BadgeTable()
 
 let template = fs.readFileSync( './src/data/template.txt', 'utf-8' )
 
-const badgeTable = btg.getTable( {
-    'preset': 'githubActivity',
-    'projects': config['npmPackages']
-} )
+const strs = Object
+    .entries( config )
+    .reduce( ( acc, [ preset, projects ]) => {
+        const badgeTable = btg.getTable( { preset, projects } )
+        acc = acc.replace( `{{${preset}}}`, badgeTable )
+        return acc
+    }, template )
 
-const strs = template.replace( '{{badgeTable}}', badgeTable)
 fs.writeFileSync( './profile/README.md', strs, 'utf-8' )
 console.log( 'README.md updated successfully.')
