a89deccb259c247d377d7054f2b64f11238af4ea
αηdr3α5 <AndreasBanholzer@Me.com>
2025-06-30 16:56:22 +0200

    Update config.mjs

--- src/data/config.mjs
+++ src/data/config.mjs
@@ -24,6 +24,14 @@ const config = {
             "imageUrl": "",
             "uptimeRobotId": "m800841892-66d45cc47d17f8587c3c969f",
             "url": "https://community.flowmcp.org"
+        },
+        {
+            "title": "website",
+            "githubUserName": "flowMCP",
+            "githubRepository": "www-flowmcp-org",
+            "imageUrl": "",
+            "uptimeRobotId": "m800843065-01b21fc8a0c00272f7e08839",
+            "url": "https://community.flowmcp.org"
         }
     ]
 } 
