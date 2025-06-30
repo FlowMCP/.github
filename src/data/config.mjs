6da029e1d590e70c68db2f4629993fd96188720f
αηdr3α5 <AndreasBanholzer@Me.com>
2025-06-30 17:08:41 +0200

    Update config.mjs

--- src/data/config.mjs
+++ src/data/config.mjs
@@ -1,40 +1,61 @@
-const config = {
-    'githubDetails': [
-        {
-            "title": "flowmcp-core",
-            "githubUserName": "FlowMCP",
-            "githubRepository": "flowMCP-core"
-        },
-        {
-            "title": "flowmcp-schemas",
-            "githubUserName": "flowMCP",
-            "githubRepository": "flowMCP-schemas"
-        },
-        {
-            "title": "flowmcp-servers",
-            "githubUserName": "flowMCP",
-            "githubRepository": "flowMCP-servers"
-        }
-    ],
-    'documentationRepository': [
-        {
-            "title": "community-sever",
-            "githubUserName": "flowMCP",
-            "githubRepository": "community-server",
-            "imageUrl": "",
-            "uptimeRobotId": "m800841892-66d45cc47d17f8587c3c969f",
-            "url": "https://community.flowmcp.org"
-        },
-        {
-            "title": "website",
-            "githubUserName": "flowMCP",
-            "githubRepository": "www-flowmcp-org",
-            "imageUrl": "",
-            "uptimeRobotId": "m800843065-01b21fc8a0c00272f7e08839",
-            "url": "https://www.flowmcp.org"
-        }
-    ]
-} 
-
-
-export { config }
+const config = [
+    {
+        'replace': '{{githubDetails}}',
+        'preset': 'githubDetails',
+        'projects': [
+            {
+                "title": "flowmcp-core",
+                "githubUserName": "FlowMCP",
+                "githubRepository": "flowMCP-core"
+            },
+            {
+                "title": "flowmcp-schemas",
+                "githubUserName": "flowMCP",
+                "githubRepository": "flowMCP-schemas"
+            },
+            {
+                "title": "flowmcp-servers",
+                "githubUserName": "flowMCP",
+                "githubRepository": "flowMCP-servers"
+            }
+        ]
+    },
+    {
+        'replace': '{{documentationRepository}}',
+        'preset': 'documentationRepository',
+        'projects': [
+            {
+                "title": "community-sever",
+                "githubUserName": "flowMCP",
+                "githubRepository": "community-server",
+                "imageUrl": "",
+                "uptimeRobotId": "m800841892-66d45cc47d17f8587c3c969f",
+                "url": "https://community.flowmcp.org"
+            },
+            {
+                "title": "website",
+                "githubUserName": "flowMCP",
+                "githubRepository": "www-flowmcp-org",
+                "imageUrl": "",
+                "uptimeRobotId": "m800843065-01b21fc8a0c00272f7e08839",
+                "url": "https://www.flowmcp.org"
+            }
+        ]
+    },
+    {
+        'replace': '{{x402}}',
+        'preset': 'githubDetails',
+        'projects': [
+            {
+                "title": "x402-core",
+                "githubUserName": "FlowMCP",
+                "githubRepository": "x402-core"
+            },
+                        {
+                "title": "x402-mcp-middleware",
+                "githubUserName": "FlowMCP",
+                "githubRepository": "x402-mcp-middlewaree"
+            }
+        ]
+    }
+]
