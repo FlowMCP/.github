3695767c3db1f2866078af50c8fd43c5cef9b4a1
αηdr3α5 <AndreasBanholzer@Me.com>
2025-07-02 00:52:45 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -7,7 +7,22 @@ let template = fs.readFileSync( './src/data/template.txt', 'utf-8' )
 
 const strs = config
     .reduce( ( acc, { preset, projects, replace } ) => {
-        const badgeTable = btg.getTable( { preset, projects } )
+        const badgeTable = btg
+            .getTable( { preset, projects } )
+            .split( "\n" )
+            .reverse()
+            .reduce( ( acc, line, index ) => {
+                if( line.startsWith( '|' ) && acc.removed < 2 ) {
+                    acc.removed += 1
+                } else {
+                    acc.lines.push( line )
+                }
+        
+                return acc
+            }, { lines: [], removed: 0 } )
+            .lines
+            .reverse()
+
         acc = acc.replace( replace, badgeTable )
         return acc
     }, template )
