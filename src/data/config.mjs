bd00d03271703a09dcaba2ac1a8a7840fe811f2f
αηdr3α5 <AndreasBanholzer@Me.com>
2025-08-22 20:52:20 +0200

    Update config.mjs

--- src/data/config.mjs
+++ src/data/config.mjs
@@ -33,7 +33,8 @@ const config = [
                 "githubRepository": "community-server",
                 "imageUrl": "",
                 "uptimeRobotId": "m800841892-66d45cc47d17f8587c3c969f",
-                "url": "https://community.flowmcp.org"
+                "url": "https://community.flowmcp.org",
+                "githubWorkflowPath": "test-on-release.yml"
             },
             {
                 "title": "website",
@@ -41,7 +42,8 @@ const config = [
                 "githubRepository": "www-flowmcp-org",
                 "imageUrl": "",
                 "uptimeRobotId": "m800843065-01b21fc8a0c00272f7e08839",
-                "url": "https://www.flowmcp.org"
+                "url": "https://www.flowmcp.org",
+                "githubWorkflowPath": "test-on-release.yml"
             }
         ]
     },
